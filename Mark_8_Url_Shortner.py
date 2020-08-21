9781d4b5441c31568099e72c0493da3618439917
saidudeprakash <67602636+saidudeprakash@users.noreply.github.com>
2020-08-21 16:13:35 +0530

    Update Mark_8_Url_Shortner.py

--- Mark_8_Url_Shortner.py
+++ Mark_8_Url_Shortner.py
@@ -2,6 +2,8 @@ import tkinter
 from tkinter import *
 import pyshorteners
 root=Tk()
+root.geometry('400x400')
+root.title('URL_SHORTENER')
 def get_url():
     label1=Label(root,text=pyshorteners.Shortener().tinyurl.short(url.get()))
     label1.pack()
